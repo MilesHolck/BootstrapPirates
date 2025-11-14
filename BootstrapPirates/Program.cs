4cd620bb0d5d950e9e77bebfc88080d4384e6959
Ida Emilie Balch Hansen <idh004@edu.zealand.dk>
2025-11-14 12:25:29 +0100

    Min gren

--- BootstrapPirates/Program.cs
+++ BootstrapPirates/Program.cs
@@ -6,3 +6,6 @@ Console.WriteLine("Bootstrap Pirates is setting sail!");
 Console.WriteLine("OHØJ");
 
 Console.WriteLine("Ready to bootstrap your .NET projects with ease!");
+
+
+Console.WriteLine("Jeg har lavet min egen gren hehe");
