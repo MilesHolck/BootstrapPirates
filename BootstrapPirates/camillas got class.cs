f183c5e8f334dda6082e6627d9ead751f253279a
camil <camilla-holck@hotmail.com>
2025-11-14 12:35:35 +0100

    branch test

---
+++ BootstrapPirates/camillas got class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapPirates
+{
+    public class camillas_got_class
+    {
+        public void test()
+        {
+            Console.WriteLine("camillas got class");
+        }
+    }
+}
